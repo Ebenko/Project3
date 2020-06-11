f7a0083d72232662219dd74bac33967aa89a655c
Ethel <ethel.maria.benko@gmail.com>
2020-06-11 13:29:55 +0200

    Initial commit

---
+++ JS.js
@@ -0,0 +1,12 @@
+document.querySelector('.banner button').addEventListener
+('click', ()=> {
+
+    document.querySelector('.banner')
+    .style.display = 'none';
+
+    document.querySelector('.slideshow-wrapper')
+    .style.cssText = 'opacity: 1; visibility: visible';
+    
+    document.querySelector('.slideshow').style.animation = 'slideshow 20s infinite';
+
+});
